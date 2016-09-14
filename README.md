8f327fd6106e5a12877aa51c2f66474cca4fd650
alexander.bondarenko <alexander.bondarenko@itrexgroup.com>
2016-09-14 16:06:14 +0300

    gulpfile

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# a-greed
-custom greed system created by Alexanders
+# a-grid
+Custom greed system created by Alexanders
+
+#Equal
+
+.a-equal-h -- elements that will have the same height
+
+.a-equal-parent -- parent element of .a-equal-h
+
+.a-equal-parent data-min/maxWidth -- make calculations by upper/lower display size
---
+++ resources/js/equal/equal.js
@@ -0,0 +1,54 @@
+/**
+ * Created by alexanderbondarenko on 9/14/16.
+ */
+
+function Equal() {
+}
+
+Equal.prototype.calculate = function () {
+    var self = this,
+        parents = $('.a-equal-parent'),
+        width = $(window).outerWidth(),
+        children, height, childH, maxWidth, minWidth;
+
+    if (parents.length) {
+        parents.each(function () {
+            maxWidth = $(this).attr('data-maxWidth') || width + 1;
+            minWidth = $(this).attr('data-minWidth') || 0;
+            children = $(this).find('.a-equal-h');
+            children.css('height', '');
+
+            if (width < maxWidth && width > minWidth){
+                height = $(children[0]).outerHeight();
+
+                children.each(function () {
+                    childH = $(this).outerHeight();
+
+                    if (childH > height) {
+                        height = childH;
+                    }
+                });
+
+                children.css('height', height);
+            }
+        })
+    }
+};
+
+Equal.prototype.events = function () {
+    var self = this;
+
+    $(window).resize(function () {
+        self.calculate();
+    });
+
+    $(document).ready(function () {
+        self.calculate();
+    })
+};
+
+Equal.prototype.init = function () {
+    this.events();
+};
+
+module.exports = new Equal();
